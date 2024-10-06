f29f5ed47f3ec50e688424760772c3fa41162e45
ltxlong <416803647@qq.com>
2024-10-06 14:26:40 +0800

    Add files via upload

---
+++ alist/start.sh
@@ -0,0 +1,64 @@
+SCRIPT_PATH=$(realpath "$0")
+WORKDIR=$(dirname "${SCRIPT_PATH}")
+cd "${WORKDIR}"
+FILES_PATH=${FILES_PATH:-./}
+CURRENT_VERSION=''
+RELEASE_LATEST=''
+CMD="$@"
+
+get_current_version() {
+    chmod +x ./web.js 2>/dev/null
+    CURRENT_VERSION=$(./web.js version | grep -o v[0-9]*\.*.)
+}
+
+get_latest_version() {
+    # Get latest release version number
+    RELEASE_LATEST="$(curl -IkLs -o ${TMP_DIRECTORY}/NUL -w %{url_effective} https://github.com/uubulb/alist-freebsd/releases/latest | grep -o "[^/]*$")"
+    RELEASE_LATEST="v${RELEASE_LATEST#v}"
+    if [[ -z "$RELEASE_LATEST" ]]; then
+        echo "error: Failed to get the latest release version, please check your network."
+        exit 1
+    fi
+}
+
+download_web() {
+    DOWNLOAD_LINK="https://github.com/uubulb/alist-freebsd/releases/latest/download/alist"
+    if ! wget -qO "$ZIP_FILE" "$DOWNLOAD_LINK"; then
+        echo 'error: Download failed! Please check your network or try again.'
+        return 1
+    fi
+    return 0
+}
+
+install_web() {
+    install -m 755 ${TMP_DIRECTORY}/alist ${FILES_PATH}/web.js
+}
+
+run_web() {
+    nohup killall web.js > /dev/null
+    chmod +x ./web.js
+    exec ./web.js server > /dev/null 2>&1 &
+}
+
+TMP_DIRECTORY="$(mktemp -d)"
+ZIP_FILE="${TMP_DIRECTORY}/alist"
+
+get_current_version
+get_latest_version
+if [ "${RELEASE_LATEST}" = "${CURRENT_VERSION}" ]; then
+    "rm" -rf "$TMP_DIRECTORY"
+    run_web
+    exit
+fi
+download_web
+EXIT_CODE=$?
+if [ ${EXIT_CODE} -eq 0 ]; then
+    :
+else
+    "rm" -rf "$TMP_DIRECTORY"
+    run_web
+    exit
+fi
+install_web
+"rm" -rf "$TMP_DIRECTORY"
+run_web
---
+++ install_alist.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# 判断是否在 public_nodejs 目录下
+if [ "$(basename $(pwd))" == "public_nodejs" ]; then
+    # 删除 public/index.html 文件，如果不存在则跳过
+    [ -f public/index.html ] && rm public/index.html
+    
+    # 下载所需文件
+    wget https://raw.githubusercontent.com/ltxlong/serv00-alist/main/alist/app.js 
+    wget https://raw.githubusercontent.com/ltxlong/serv00-alist/main/alist/start.sh
+    wget https://raw.githubusercontent.com/ltxlong/serv00-alist/main/alist/package.json
+    wget https://github.com/uubulb/alist-freebsd/releases/latest/download/alist
+    
+    # 重命名并设置权限，运行服务
+    mv alist web.js && chmod +x web.js && ./web.js server
+
+    # 清屏
+    clear
+    
+    # 显示安装完成信息
+    echo "已成功安装 Alist！请按照以下步骤进行配置："
+
+    # 提示修改 app.js 文件中的端口号
+    echo "1. 请修改 app.js 文件中的第 13 行，将 PORT 替换为您实际开放的端口号。"
+    echo "（如果还想要开启 aria2，那么将 app.js 的第43~62行注释打开，并且修改文件中第 50 行，将 PORT 替换为您实际开放的另一个端口号）"
+    
+    # 提示修改配置文件
+    echo "2. 请修改 data/config.json 的配置文件，以确保所有设置符合您的需求。"
+
+else
+    # 显示错误信息
+    echo "检测到未在public_nodejs目录下，请确认网站添加的类型是否为nodejs"
+fi
